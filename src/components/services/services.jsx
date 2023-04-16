ebb8547f771cefa8f71bbf7060b227438374f307
Miguel Jesus <miguelclaudiojesus@gmail.com>
2023-04-16 14:57:52 +0100

    update

---
+++ src/components/services/services.jsx
@@ -0,0 +1,129 @@
+import React from 'react'
+import './services.css'
+import { BiCheck } from 'react-icons/bi'
+
+const services = () => {
+    return (
+        <section id='services'>
+            <h5>Tipos de serviços</h5>
+            <h2>Que ofereço</h2>
+
+            <div className='container services__container'>
+                <article className='service'>
+                    <div className='service__head'>
+                        <h3>FronteEnd Desenvolvedor</h3>
+                    </div>
+
+                    <ul className='service__list'>
+                        <li>
+                            <BiCheck className='service__list-icon' />
+                            <p>Texto informativo.</p>
+                        </li>
+                        <li>
+                            <BiCheck className='service__list-icon' />
+                            <p>Texto informativo.</p>
+                        </li>
+                        <li>
+                            <BiCheck className='service__list-icon' />
+                            <p>Texto informativo.</p>
+                        </li>
+                        <li>
+                            <BiCheck className='service__list-icon' />
+                            <p>Texto informativo.</p>
+                        </li>
+                        <li>
+                            <BiCheck className='service__list-icon' />
+                            <p>Texto informativo.</p>
+                        </li>
+                        <li>
+                            <BiCheck className='service__list-icon' />
+                            <p>Texto informativo.</p>
+                        </li>
+                        <li>
+                            <BiCheck className='service__list-icon' />
+                            <p>Texto informativo.</p>
+                        </li>
+                    </ul>
+                </article>
+
+
+                <article className='service'>
+                    <div className='service__head'>
+                        <h3>FronteEnd Desenvolvedor</h3>
+                    </div>
+
+                    <ul className='service__list'>
+                        <li>
+                            <BiCheck className='service__list-icon' />
+                            <p>Texto informativo.</p>
+                        </li>
+                        <li>
+                            <BiCheck className='service__list-icon' />
+                            <p>Texto informativo.</p>
+                        </li>
+                        <li>
+                            <BiCheck className='service__list-icon' />
+                            <p>Texto informativo.</p>
+                        </li>
+                        <li>
+                            <BiCheck className='service__list-icon' />
+                            <p>Texto informativo.</p>
+                        </li>
+                        <li>
+                            <BiCheck className='service__list-icon' />
+                            <p>Texto informativo.</p>
+                        </li>
+                        <li>
+                            <BiCheck className='service__list-icon' />
+                            <p>Texto informativo.</p>
+                        </li>
+                        <li>
+                            <BiCheck className='service__list-icon' />
+                            <p>Texto informativo.</p>
+                        </li>
+                    </ul>
+                </article>
+
+                <article className='service'>
+                    <div className='service__head'>
+                        <h3>FronteEnd Desenvolvedor</h3>
+                    </div>
+
+                    <ul className='service__list'>
+                        <li>
+                            <BiCheck className='service__list-icon' />
+                            <p>Texto informativo.</p>
+                        </li>
+                        <li>
+                            <BiCheck className='service__list-icon' />
+                            <p>Texto informativo.</p>
+                        </li>
+                        <li>
+                            <BiCheck className='service__list-icon' />
+                            <p>Texto informativo.</p>
+                        </li>
+                        <li>
+                            <BiCheck className='service__list-icon' />
+                            <p>Texto informativo.</p>
+                        </li>
+                        <li>
+                            <BiCheck className='service__list-icon' />
+                            <p>Texto informativo.</p>
+                        </li>
+                        <li>
+                            <BiCheck className='service__list-icon' />
+                            <p>Texto informativo.</p>
+                        </li>
+                        <li>
+                            <BiCheck className='service__list-icon' />
+                            <p>Texto informativo.</p>
+                        </li>
+                    </ul>
+                </article>
+
+            </div>
+        </section>
+    )
+}
+
+export default services
